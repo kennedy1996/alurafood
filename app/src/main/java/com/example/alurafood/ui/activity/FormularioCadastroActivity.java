2bf389b237120d60bfe521b686e83507e1e9be1f
kennedy1996 <87372917+kennedy1996@users.noreply.github.com>
2021-08-10 15:44:02 -0300

    Criando Projeto

---
+++ app/src/main/java/com/example/alurafood/ui/activity/FormularioCadastroActivity.java
@@ -0,0 +1,16 @@
+package com.example.alurafood.ui.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.alurafood.R;
+
+public class FormularioCadastroActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_formulario_cadastro);
+    }
+}
